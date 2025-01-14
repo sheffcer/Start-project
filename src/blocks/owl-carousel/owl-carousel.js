a2d9e1e9374c2e615d5146d5f0cc310ab01dc02d
ale <ale.inertia@gmail.com>
2025-01-14 17:57:20 +0200

    added nav for catalog-carousel

--- src/blocks/owl-carousel/owl-carousel.js
+++ src/blocks/owl-carousel/owl-carousel.js
@@ -1,3 +1,40 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      items: 13, // Количество видимых слайдов
+      loop: true, // Зацикливать карусель
+      margin: 10, // Отступы между слайдами
+      autoplay: true, // Автопрокрутка
+      autoplayTimeout: 2000, // Время до следующего слайда (в миллисекундах)
+      autoplayHoverPause: true, // Пауза автопрокрутки при наведении на слайд
+      nav: true,
+      navText: ['<', '>'], // Стандартный текст для стрелок (можно заменить на что угодно)
+      navContainer: '.custom-nav', // Указываем кастомный контейнер для навигации
+      responsive : {
+              0 : {
+                items: 4,
+                // nav: true,
+                loop: true,
+                center: true,
+              },
+              480 : {
+                items: 4,
+              },
+              768 : {
+                items: 7,
+              },
+              992 : {
+                items: 10,
+              },
+              1200 : {
+                items: 13,
+              },
+              1400 : {
+                items: 13,
+              }
+            } 
+    });
+  });
+
 // $(document).ready(function(){
 
 //   $("#owl-carousel-demo").owlCarousel({
--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,15 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     // Add your custom script here
-    $(document).ready(function() {
-      $('.owl-carousel').owlCarousel({
-        items: 13, // Количество видимых слайдов
-        loop: true, // Зацикливать карусель
-        margin: 10, // Отступы между слайдами
-        autoplay: true, // Автопрокрутка
-        autoplayTimeout: 2000, // Время до следующего слайда (в миллисекундах)
-        autoplayHoverPause: true // Пауза автопрокрутки при наведении на слайд
-      });
-    });
+    // $(document).ready(function() {
+    //   $('.owl-carousel').owlCarousel({
+    //     items: 13, // Количество видимых слайдов
+    //     loop: true, // Зацикливать карусель
+    //     margin: 10, // Отступы между слайдами
+    //     autoplay: true, // Автопрокрутка
+    //     autoplayTimeout: 2000, // Время до следующего слайда (в миллисекундах)
+    //     autoplayHoverPause: true // Пауза автопрокрутки при наведении на слайд
+    //   });
+    // });
     
 });
