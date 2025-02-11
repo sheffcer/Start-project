f0749161ce928d71cf72479116e4360ac33c54fe
ale <ale.inertia@gmail.com>
2025-02-11 11:38:40 +0200

    add check carouseld

--- src/blocks/embla-carousel/embla-carousel.js
+++ src/blocks/embla-carousel/embla-carousel.js
@@ -4,6 +4,9 @@ import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton';
 
 // Инициализация для первого слайдера
 const rootNodeFirst = document.querySelector('.embla--first');
+if (!rootNodeFirst) {
+  console.error('Root node for first slider is not found');
+}else {
 const viewportNodeFirst = rootNodeFirst.querySelector('.embla__viewport');
 const dotsContainerFirst = rootNodeFirst.querySelector('.embla__dots');
 const prevButtonNodeFirst = rootNodeFirst.querySelector('.embla__prev');
@@ -46,10 +49,11 @@ updateDotsFirst();
 
 // Обновление точек при изменении слайда
 emblaFirst.on('select', updateDotsFirst);
-
+}
 
 // Инициализация для второго слайдера (без кнопок)
 const rootNodeSecond = document.querySelector('.embla--second');
+if (rootNodeSecond) {  
 const viewportNodeSecond = rootNodeSecond.querySelector('.embla__viewport');
 
 // Переменная для хранения слайдера
@@ -96,6 +100,8 @@ updateSlider();
 
 // Обновляем слайдер при изменении ширины окна
 window.addEventListener('resize', updateSlider);
+} else {console.error('Root node for second slider is not found');}
+
 
 
 // Third group slider
@@ -103,6 +109,7 @@ const OPTIONS_THIRD = { align: 'start', slidesToScroll: 2, slide: 2 }
 // const OPTIONS_THIRD = { align: 'start', slidesToScroll: 1 }
 
 const emblaNodeThird = document.querySelector('.embla--third')
+if (emblaNodeThird) {
 console.log('Root Node for Third Slider is: ', emblaNodeThird);
 const viewportNodeThird = emblaNodeThird.querySelector('.embla__viewport')
 const prevBtnNodeThird = emblaNodeThird.querySelector('.embla__button--prev')
@@ -123,6 +130,7 @@ const removeDotBtnsAndClickHandlersThird = addDotBtnsAndClickHandlers(
 
 emblaApiThird.on('destroy', removePrevNextBtnsClickHandlersThird)
 emblaApiThird.on('destroy', removeDotBtnsAndClickHandlersThird)
+} else {console.error('Root node for third slider is not found');}
 
 
 // Fourth group slider
@@ -130,6 +138,7 @@ const OPTIONS_FOURTH = { align: 'start', slidesToScroll: 2, slide: 4 }
 // const OPTIONS_FOURTH = { align: 'start', slidesToScroll: 1 }
 
 const emblaNodeFourth = document.querySelector('.embla--fourth')
+if (emblaNodeFourth) {
 console.log('Root Node for Fourth Slider is: ', emblaNodeFourth);
 const viewportNodeFourth = emblaNodeFourth.querySelector('.embla__viewport')
 const prevBtnNodeFourth = emblaNodeFourth.querySelector('.embla__button--prev')
@@ -150,6 +159,8 @@ const removeDotBtnsAndClickHandlersFourth = addDotBtnsAndClickHandlers(
 
 emblaApiFourth.on('destroy', removePrevNextBtnsClickHandlersFourth)
 emblaApiFourth.on('destroy', removeDotBtnsAndClickHandlersFourth)
+} else {console.error('Root node for fourth slider is not found');}
+
 
 
 // Five group slider
@@ -158,6 +169,7 @@ const OPTIONS_FIVE = { align: 'start', slidesToScroll: 2, slide: 2 }
 // const OPTIONS_FIVE = { align: 'start', slidesToScroll: 1 }
 
 const emblaNodeFive = document.querySelector('.embla--five')
+if (emblaNodeFive) {
 console.log('Root Node for Five Slider is: ', emblaNodeFive);
 const viewportNodeFive = emblaNodeFive.querySelector('.embla__viewport')
 const prevBtnNodeFive = emblaNodeFive.querySelector('.embla__button--prev')
@@ -178,7 +190,7 @@ const removeDotBtnsAndClickHandlersFive = addDotBtnsAndClickHandlers(
 
 emblaApiFive.on('destroy', removePrevNextBtnsClickHandlersFive)
 emblaApiFive.on('destroy', removeDotBtnsAndClickHandlersFive)
-
+} else {console.error('Root node for five slider is not found');}
 
 // Sixth group slider
 
@@ -215,6 +227,7 @@ const OPTIONS_ZERO = { align: 'start', slidesToScroll: 1, slide: 1 }
 // const OPTIONS_ZERO = { align: 'start', slidesToScroll: 1 }
 
 const emblaNodeZero = document.querySelector('.embla--zero')
+if (emblaNodeZero) {
 console.log('Root Node for Zero Slider is: ', emblaNodeZero);
 const viewportNodeZero = emblaNodeZero.querySelector('.embla__viewport')
 const prevBtnNodeZero = emblaNodeZero.querySelector('.embla__button--prev')
@@ -235,3 +248,4 @@ const removePrevNextBtnsClickHandlersZero = addPrevNextBtnsClickHandlers(
 
 emblaApiZero.on('destroy', removePrevNextBtnsClickHandlersZero)
 // emblaApiZero.on('destroy', removeDotBtnsAndClickHandlersZero)
+} else {console.error('Root node for zero slider is not found');}
